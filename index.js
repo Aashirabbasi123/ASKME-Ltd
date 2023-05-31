2e5bd401d77ef660a3881d67bf4de000824bb421
Aashirabbasi123 <133881725+Aashirabbasi123@users.noreply.github.com>
2023-05-31 18:27:14 +0500

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 document.getElementsByTagName("h1")[0].style.fontSi
+// window.addEventListener('DOMContentLoaded', function() {
+//     var video = document.getElementById('myVideo');
+//     video.play();
+//   });
+  
